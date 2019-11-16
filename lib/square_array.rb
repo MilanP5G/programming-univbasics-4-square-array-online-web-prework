e357cd8108d5a042dba0bba1bd4fcd2f8f14d284
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-11-16 18:22:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,31 @@
-def square_array(array)
-  # your code here
+#def square_array(numbers)
+ # count = 0
+    
+ #   while count < numbers.length do
+ #    numbers[count] **2
+ #   count += 1
+ #  end
+#end 
+
+results = []
+
+
+def square_array(numbers)
+  count = 0 
+  
+  while count < numbers.length do
+    numbers[counter] **2
+    count += 1
+  end
 end
+  
+
+def results_push(square_array)
+  results.push(square_array)
+end
+
+results
+
+
+
+
