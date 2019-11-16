39b5b29e9b78069b29cefb58e58514681bb68b48
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-11-16 18:23:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -10,14 +10,8 @@
 results = []
 
 def square_array(numbers)
-  count = 0 
-  
-  while count < numbers.length do
-    numbers[counter] **2
-    count += 1
-  end
+  numbers[1]**2
 end
-  
 
 def results_push(square_array)
   results.push(square_array)
